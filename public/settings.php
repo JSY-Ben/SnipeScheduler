4bb577b6805e3fb4b64e4914b60bf9c681b80b90
JSY-Ben <cheesypz@hotmail.com>
2026-01-16 19:17:25 +0000

    Changed Tab Styling on new Admin page

--- public/settings.php
+++ public/settings.php
@@ -624,12 +624,9 @@ $allowedCategoryIds = array_map('intval', $allowedCategoryIds);
             </div>
         <?php endif; ?>
 
-        <ul class="nav nav-tabs mb-3">
+        <ul class="nav nav-tabs reservations-subtabs mb-3">
             <li class="nav-item">
                 <a class="nav-link active" href="#admin-settings">Settings</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#admin-access">Access &amp; Roles</a>
             </li>
         </ul>
 
