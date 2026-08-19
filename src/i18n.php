cda5d4172c9d94e42c18c7d6bc8da41148513365
JSY-Ben <cheesypz@hotmail.com>
2026-08-19 09:18:05 +0100

    Added Fallback for Locale Changes that don't have server side support

--- src/i18n.php
+++ src/i18n.php
@@ -1,8 +1,45 @@
 <?php
 
+function i18n_locale_candidates(string $language): array
+{
+    $normalizedLanguage = str_replace('-', '_', trim($language));
+    $normalizedLanguage = $normalizedLanguage !== '' ? $normalizedLanguage : 'C';
+
+    if (strtoupper($normalizedLanguage) === 'C' || strtoupper($normalizedLanguage) === 'POSIX') {
+        return [$normalizedLanguage, 'C.UTF-8', 'C'];
+    }
+
+    $hyphenatedLanguage = str_replace('_', '-', $normalizedLanguage);
+    $baseLanguage = strtok($normalizedLanguage, '_') ?: $normalizedLanguage;
+    $candidateLocales = [];
+    foreach (array_unique([$normalizedLanguage, $hyphenatedLanguage, $baseLanguage]) as $localeName) {
+        $candidateLocales[] = $localeName . '.UTF-8';
+        $candidateLocales[] = $localeName . '.utf8';
+        $candidateLocales[] = $localeName . '.UTF8';
+        $candidateLocales[] = $localeName;
+        $candidateLocales[] = $localeName . '.ISO8859-15';
+        $candidateLocales[] = $localeName . '.ISO8859-1';
+    }
+
+    return array_values(array_unique(array_filter($candidateLocales)));
+}
+
 $config = load_config();
-$locale = ($config['app']['language'] ?? 'C') . '.UTF-8';
-putenv('LANG=' . $locale);
-setlocale(LC_ALL, $locale);
+$language = trim((string)($config['app']['language'] ?? 'C'));
+$language = $language !== '' ? $language : 'C';
+$normalizedLanguage = str_replace('-', '_', $language);
+$candidateLocales = i18n_locale_candidates($normalizedLanguage);
+$activeLocale = false;
+foreach ($candidateLocales as $candidateLocale) {
+    $activeLocale = setlocale(LC_ALL, $candidateLocale);
+    if ($activeLocale !== false) {
+        break;
+    }
+}
+
+putenv('LANG=' . ($activeLocale !== false ? $activeLocale : $candidateLocales[0]));
+putenv('LC_ALL=' . ($activeLocale !== false ? $activeLocale : $candidateLocales[0]));
+putenv('LANGUAGE=' . $normalizedLanguage);
 bindtextdomain('messages', SRC_PATH . '/locales');
+bind_textdomain_codeset('messages', 'UTF-8');
 textdomain('messages');
